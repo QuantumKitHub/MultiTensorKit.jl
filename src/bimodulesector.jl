e272b98b457805d4bff90a003b78f2002267e7a0
Boris De Vos <boris.devos@ugent.be>
2025-03-10 15:02:54 +0100

    update artifact_path constant

--- src/bimodulesector.jl
+++ src/bimodulesector.jl
@@ -48,7 +48,7 @@ end
 
 # Data from files
 # ---------------
-const artifact_path = joinpath(artifact"fusiondata", "MultiTensorKit.jl-data-v0.1.0")
+const artifact_path = joinpath(artifact"fusiondata", "MultiTensorKit.jl-data-v0.1.1")
 
 function extract_Nsymbol(::Type{A4Object})
     filename = joinpath(artifact_path, "A4", "Nsymbol.json")
