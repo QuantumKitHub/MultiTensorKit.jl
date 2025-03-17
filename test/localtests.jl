7743a008100e6be2ae020f087435ef8ea4d6018f
Boris De Vos <boris.devos@ugent.be>
2025-03-17 10:09:24 +0100

    more local tests

--- test/localtests.jl
+++ test/localtests.jl
@@ -1,5 +1,5 @@
 using TensorKitSectors
-using .MultiTensorKit
+using MultiTensorKit
 using Revise
 
 testobj = A4Object(1,1,1) # fusion cat object
@@ -17,7 +17,7 @@ one(testmodobj)
 leftone(testmodobj)
 rightone(testmodobj)
 
-Fsymbol(testobj, testobj, testobj, testobj, testobj, testobj)
+Fsymbol(testobj, testobj, A4Object(1,1,3), testobj, A4Object(1,1,3), A4Object(1,1,4))
 
 using Artifacts
 using DelimitedFiles
@@ -27,14 +27,15 @@ filename = joinpath(artifact_path, "A4", "Fsymbol_4.txt")
 txt_string = read(filename, String)
 F_arraypart = copy(readdlm(IOBuffer(txt_string)));
 
+MTK = MultiTensorKit
 F_arraypart = MTK.convert_Fs(F_arraypart)
-for (color, colordict) in F_arraypart
-    for (labels, F) in colordict
-        if length(F) == 2
-            println(color, labels, F)
-        end
-    end
-end
+# for (color, colordict) in F_arraypart
+#     for (labels, F) in colordict
+#         if length(F) == 2
+#             println(color, labels, F)
+#         end
+#     end
+# end
 i,j,k,l = (4,12,12,2) # 5,2,8,10
 a,b,c,d,e,f = (1, 11, 3, 1, 1, 3) #(2,1,1,2,2,3)
 testF = F_arraypart[(i,j,k,l)][(a,b,c,d,e,f)]
@@ -51,13 +52,32 @@ map!(result, reshape(testF, size(result))) do pair
     return pair[2]
 end
 
+for c in testF[2]
+    println(c)
+end
+
 function bla()
     return for (k,v) in F_arraypart
         @show k
     end
 end
 
-using JSON3
-filename2 = joinpath(artifact_path, "A4", "Fsymbol_1.json")
-json_string = read(filename2, String)
-Farray_part2 = copy(JSON3.read(json_string));
+N = MultiTensorKit._get_Ncache(A4Object);
+
+duals = MultiTensorKit._get_dual_cache(A4Object)[2]
+# checking duals is correct
+for i in 1:12, j in 1:12
+    for (index, a) in enumerate(duals[i,j])
+        aob = A4Object(i,j,index)
+        bob = A4Object(j,i,a)
+        leftone(aob) ∈ aob ⊗ bob && rightone(aob) ∈ bob ⊗ aob || @show i,j,aob,bob
+    end
+end
+
+A = MultiTensorKit._get_Fcache(A4Object)
+a,b,c,d,e,f = A4Object(1,1,3), A4Object(1,1,2), A4Object(1,1,2), A4Object(1,1,2), A4Object(1,1,2), A4Object(1,1,2)
+a,b,c,d,e,f = A4Object(1,1,1), A4Object(1,1,2), A4Object(1,1,2), A4Object(1,1,1), A4Object(1,1,2), A4Object(1,1,4)
+coldict = A[a.i][a.i, a.j, b.j, c.j]
+bla = get(coldict, (a.label, b.label, c.label, d.label, e.label, f.label)) do
+        return coldict[(a.label, b.label, c.label, d.label, e.label, f.label)]
+    end
