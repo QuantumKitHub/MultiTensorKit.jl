7222cc86905ffc1a414f2689ce744ffb96a11cf5
Boris De Vos <boris.devos@ugent.be>
2025-06-20 16:31:45 +0200

    use math engine

--- docs/make.jl
+++ docs/make.jl
@@ -6,13 +6,20 @@ pages = ["Home" => "index.md", "Library" => "lib/library.md",
          "Manual" => ["man/fusioncats.md", "man/multifusioncats.md"],
          "References" => "references.md"]
 
-
 # bibliography
 bibpath = joinpath(@__DIR__, "src", "assets", "MTKrefs.bib")
 bib = CitationBibliography(bibpath; style=:authoryear)
 
-makedocs(; sitename="MultiTensorKit Documentation", modules=[MultiTensorKit],
+mathengine = MathJax3(Dict(:loader => Dict("load" => ["[tex]/physics"]),
+                           :tex => Dict("inlineMath" => [["\$", "\$"], ["\\(", "\\)"]],
+                                        "tags" => "ams",
+                                        "packages" => ["base", "ams", "autoload", "physics",
+                                                       "mathtools"])))
+
+makedocs(; sitename="MultiTensorKit.jl", modules=[MultiTensorKit],
+         #  assets=["assets/custom.css"],
          authors="Boris De Vos, Laurens Lootens and Lukas Devos",
-         pages=pages, pagesonly=true, plugins=[bib])
+         pages=pages, pagesonly=true, plugins=[bib],
+         format=Documenter.HTML(; mathengine=mathengine))
 
 deploydocs(; repo="https://github.com/QuantumKitHub/MultiTensorKit.jl")
