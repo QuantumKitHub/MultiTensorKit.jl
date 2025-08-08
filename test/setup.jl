db4b577dd3522d077774d41bfa7fd7a0d8459588
Boris De Vos <boris.devos@ugent.be>
2025-08-08 12:50:22 +0200

    introduce test setup file

---
+++ test/setup.jl
@@ -0,0 +1,29 @@
+using MultiTensorKit
+using TensorKitSectors, TensorKit
+using Test, TestExtras
+
+function unitarity_test(as::Vector{I}, bs::Vector{I}, cs::Vector{I}) where {I<:BimoduleSector}
+    @assert all(a.j == b.i for a in as, b in bs)
+    @assert all(b.j == c.i for b in bs, c in cs)
+    
+    for a in as, b in bs, c in cs
+        for d in ⊗(a, b, c)
+            es = collect(intersect(⊗(a, b), map(dual, ⊗(c, dual(d)))))
+            fs = collect(intersect(⊗(b, c), map(dual, ⊗(dual(d), a))))
+            Fblocks = Vector{Any}()
+            for e in es
+                for f in fs
+                    Fs = Fsymbol(a, b, c, d, e, f)
+                    push!(Fblocks,
+                          reshape(Fs,
+                                  (size(Fs, 1) * size(Fs, 2),
+                                   size(Fs, 3) * size(Fs, 4))))
+                end
+            end
+            F = hvcat(length(fs), Fblocks...)
+            isapprox(F' * F, one(F); atol=1e-12, rtol=1e-12) || return false
+        end
+    end
+    return true
+end
+
