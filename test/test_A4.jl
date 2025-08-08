662efd808b57392b3a9f504258657ea2115b58d5
Boris De Vos <boris.devos@ugent.be>
2025-08-08 15:57:48 +0200

    finish up sector tests

--- test/test_A4.jl
+++ test/test_A4.jl
@@ -2,26 +2,103 @@ I = A4Object
 Istr = TensorKitSectors.type_repr(I)
 r = size(I)
 
+println("----------------------")
+println("|    Sector tests    |")
+println("----------------------")
+
 @testset "$Istr Basic type properties" verbose = true begin
     @test eval(Meta.parse(sprint(show, I))) == I
     @test eval(Meta.parse(TensorKitSectors.type_repr(I))) == I
 end
 
-@testset "$Istr Fusion Category $i" for i in 1:r
-    objects = I.(i, i, MTK._get_dual_cache(I)[2][i, i])
-
-    @testset "Basic properties" begin
-        s = rand(objects, 3)
-        @test eval(Meta.parse(sprint(show, s[1]))) == s[1]
-        @test @constinferred(hash(s[1])) == hash(deepcopy(s[1]))
-        @test isone(@constinferred(one(s[1])))
-        @constinferred dual(s[1])
-        @constinferred dim(s[1])
-        @constinferred frobeniusschur(s[1])
-        @constinferred Bsymbol(s...)
-        @constinferred Fsymbol(s..., s...)
+@testset "$Istr: Value iterator" begin
+    @test eltype(values(I)) == I
+    @test_throws ArgumentError one(I)
+    sprev = I(1, 1, 1) # first in SectorValues
+    for (i, s) in enumerate(values(I))
+        @test !isless(s, sprev) # confirm compatibility with sort order
+        @test s == @constinferred (values(I)[i])
+        @test findindex(values(I), s) == i
+        sprev = s
+        i >= 10 && break
+    end
+    @test I(1, 1, 1) == first(values(I))
+    @test (@constinferred findindex(values(I), I(1, 1, 1))) == 1
+    for s in collect(values(I))
+        @test (@constinferred values(I)[findindex(values(I), s)]) == s
     end
 end
+
+@testset "$Istr ($i, $j) basic properties" for i in 1:r, j in 1:r
+    Cii_obs = I.(i, i, MTK._get_dual_cache(I)[2][i, i])
+    Cij_obs = I.(i, j, MTK._get_dual_cache(I)[2][i, j])
+    Cji_obs = I.(j, i, MTK._get_dual_cache(I)[2][j, i])
+    Cjj_obs = I.(j, j, MTK._get_dual_cache(I)[2][j, j])
+    c, m, mop, d = rand(Cii_obs), rand(Cij_obs), rand(Cji_obs), rand(Cjj_obs)
+
+    if i == j
+        @testset "Basic fusion properties" begin
+            s = rand(Cii_obs, 3)
+            @test eval(Meta.parse(sprint(show, s[1]))) == s[1]
+            @test @constinferred(hash(s[1])) == hash(deepcopy(s[1]))
+            @test isone(@constinferred(one(s[1])))
+            u = I.(i, i, MTK._get_dual_cache(I)[1][i])
+            @test u == @constinferred(leftone(u)) == @constinferred(rightone(u)) ==
+                @constinferred(one(u))
+            @test isone(@constinferred(one(s[1])))
+            @constinferred dual(s[1])
+            @test dual(s[1]) == I.(i, i, MTK._get_dual_cache(I)[2][i, i][s[1].label])
+            @constinferred dim(s[1])
+            @constinferred frobeniusschur(s[1])
+            @constinferred Bsymbol(s...)
+            @constinferred Fsymbol(s..., s...)
+        end
+    else
+        @testset "Basic module properties" begin
+            @test eval(Meta.parse(sprint(show, m))) == m
+            @test @constinferred(hash(m)) == hash(deepcopy(m))
+
+            @test (isone(@constinferred(leftone(m))) && isone(@constinferred(rightone(m))))
+            @test one(c) == leftone(m) == rightone(mop)
+            @test one(d) == rightone(m) == leftone(mop)
+            @test_throws DomainError one(m)
+            @test_throws DomainError one(mop)
+
+            @constinferred dual(m)
+            @test dual(m) == I.(j, i, MTK._get_dual_cache(I)[2][i, j][m.label])
+            @test dual(dual(m)) == m
+
+            @constinferred dim(m)
+            @constinferred frobeniusschur(m)
+            @constinferred Bsymbol(m, mop, c)
+            @constinferred Fsymbol(mop, m, mop, mop, d, c)
+        end
+
+        @testset "$Istr Fusion rules" begin
+            argerr = ArgumentError("invalid fusion channel")
+            # forbidden fusions
+            for obs in [(c, d), (d, c), (m, m), (mop, mop), (d, m), (m, c), (mop, d), (c, mop)]
+                @test_throws AssertionError("a.j == b.i") isempty(⊗(obs...))
+                @test_throws argerr Nsymbol(obs..., rand([c, m, mop, d]))
+            end
+
+            # allowed fusions
+            for obs in [(c, c), (d, d), (m, mop), (mop, m), (c, m), (mop, c), (m, d), (d, mop)]
+                @test !isempty(⊗(obs...))
+            end
+
+            @test Nsymbol(c, one(c), c) == Nsymbol(d, one(d), d) == 1
+
+            @test_throws argerr Nsymbol(m, mop, d)
+            @test_throws argerr Nsymbol(mop, m, c)
+            @test_throws argerr Fsymbol(m, mop, m, mop, c, d)
+        end
+    end
+end
+
+println("-----------------------------")
+println("|    F-symbol data tests    |")
+println("-----------------------------")
 
 for i in 1:r, j in 1:r
     @testset "Unitarity of $Istr F-move ($i, $j)" begin
@@ -46,7 +123,7 @@ for i in 1:r, j in 1:r
         @testset "Unitarity of right module F-move ($i, $j)" begin
             @test unitarity_test(mod_objects, right_fusion_objects, right_fusion_objects)
         end
-    
+
         # C x M x D -> M or D x Mop x C -> Mop
         @testset "Unitarity of bimodule F-move ($i, $j)" begin
             @test unitarity_test(left_fusion_objects, mod_objects, right_fusion_objects)
@@ -92,23 +169,14 @@ end
     end
 end
 
-@testset "$Istr ($i, $j) units and duals" for i in 1:r, j in 1:r
-    Cij_obs = I.(i, j, MTK._get_dual_cache(I)[2][i, j])
-
-    s = rand(Cij_obs)
-    @test eval(Meta.parse(sprint(show, s))) == s
-    @test @constinferred(hash(s)) == hash(deepcopy(s))
-    @test i == j ? isone(@constinferred(one(s))) :
-          (isone(@constinferred(leftone(s))) && isone(@constinferred(rightone(s))))
-    @constinferred dual(s)
-    @test dual(s) == I.(j, i, MTK._get_dual_cache(I)[2][i, j][s.label])
-    @test dual(dual(s)) == s
-end
+println("-----------------------------")
+println("|    F-symbol data tests    |")
+println("-----------------------------")
 
 @testset "$Istr ($i, $j) left and right units" for i in 1:r, j in 1:r
     Cij_obs = I.(i, j, MTK._get_dual_cache(I)[2][i, j])
 
-    s = rand(Cij_obs, 1)[1]
+    s = rand(Cij_obs)
     sp = Vect[I](s => 1)
     W = sp ← sp
     for T in (Float32, ComplexF64)
